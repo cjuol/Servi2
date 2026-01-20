2aa5f0f320135c5248eae7371295e3a05935d5ea
Cristóbal Jurado Oller <c.jurado.oller@gmail.com>
2026-01-20 11:36:29 +0100

    Install filament  and  a seed for user access

--- development/database/seeders/DatabaseSeeder.php
+++ development/database/seeders/DatabaseSeeder.php
@@ -15,11 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // Create admin user
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@admin.es',
+            'password' => 'password123',
         ]);
+
+        // User::factory(10)->create();
     }
 }
